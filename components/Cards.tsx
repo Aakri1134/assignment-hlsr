23709e395f302fa1f3ee69ceb9f2722aa61a2bc8
Aakri1134 <aakrisri55@gmail.com>
2025-05-05 23:04:00 +0530

    Add dropdowns

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,4 +1,76 @@
-const Card = () => {
-    
+import styles from "@/style/styles"
+import { meaning } from "@/types/types"
+import { useRef, useState } from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import { screenWidth } from "@/utils/Constants"
+
+const Card = ({ partOfSpeech, definitions, synonyms, antonyms }: meaning) => {
+  const means = useRef<number>(0)
+  const syns = useRef<number>(0)
+  const ants = useRef<number>(0)
+
+  const [isExpanded, setIsExpanded] = useState<Boolean>(false)
+
+  means.current = 0
+  syns.current = 0
+  ants.current = 0
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "blue",
+        padding: 10,
+        width : screenWidth
+      }}
+    >
+      <TouchableOpacity
+        style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+        onPressOut={() => setIsExpanded(x => !x)}
+      >
+        <Ionicons name="arrow-forward-circle" size={24} color="black" style={{transform : [{ rotate: isExpanded ? '90deg' : '0deg' }] }}/>
+        <Text style={styles.text.heading}>{partOfSpeech.toUpperCase()}</Text>
+      </TouchableOpacity>
+      {(isExpanded)? <>
+      {definitions.map((definition) => {
+        means.current = means.current + 1
+        return (
+          <View key={means.current}>
+            <Text style={styles.text.subHeading}>
+              Meaning {means.current} :
+            </Text>
+            <Text>{definition.definition}</Text>
+          </View>
+        )
+      })}
+      <Text style={styles.text.subHeading}>Synonyms : </Text>
+      {synonyms.length > 0 ? (
+        synonyms.map((synonym) => {
+          syns.current = syns.current + 1
+          return (
+            <Text key={syns.current}>
+              {syns.current}. {synonym}
+            </Text>
+          )
+        })
+      ) : (
+        <Text>No Synonyms Found</Text>
+      )}
+      <Text style={styles.text.subHeading}>Antonyms : </Text>
+      {antonyms.length > 0 ? (
+        antonyms.map((antonym) => {
+          ants.current = ants.current + 1
+          return (
+            <Text key={ants.current}>
+              {ants.current}. {antonym}
+            </Text>
+          )
+        })
+      ) : (
+        <Text>No Antonyms Found</Text>
+      )}
+      </> : null}
+    </View>
+  )
 }
 export default Card
--- components/Output.tsx
+++ components/Output.tsx
@@ -2,12 +2,11 @@ import styles from "@/style/styles"
 import { meaning, noMeaning } from "@/types/types"
 import { useEffect, useRef } from "react"
 import { Text, View } from "react-native"
+import Card from "./Cards"
 
 const Output = ({ data }: { data: meaning[] | null }) => {
-  const num = useRef<number>(0)
-  const means = useRef<number>(0)
-  const syns = useRef<number>(0)
-  const ants = useRef<number>(0)
+  const Keys = useRef<number>(0)
+
   return (
     <View
       style={{
@@ -17,65 +16,20 @@ const Output = ({ data }: { data: meaning[] | null }) => {
       }}
     >
       {data != null
-        ? data.map((meaning: meaning) => {
-            const { partOfSpeech, definitions, synonyms, antonyms }: meaning =
-              meaning
-            num.current = num.current + 1
-            means.current = 0
-            syns.current = 0
-            ants.current = 0
-            return (
-              <View
-                key={num.current}
-                style={{
-                  flex: 1,
-                  backgroundColor: "blue",
-                  padding: 10,
-                }}
-              >
-                <Text style={styles.text.heading}>
-                  {partOfSpeech.toUpperCase()}
-                </Text>
-                {definitions.map((definition) => {
-                  means.current = means.current + 1
-                  return (
-                    <View key={means.current}>
-                      <Text style={styles.text.subHeading}>
-                        Meaning {means.current} :
-                      </Text>
-                      <Text>{definition.definition}</Text>
-                    </View>
-                  )
-                })}
-                <Text style={styles.text.subHeading}>Synonyms : </Text>
-                {synonyms.length > 0 ? (
-                  synonyms.map((synonym) => {
-                    syns.current = syns.current + 1
-                    return (
-                      <Text key={syns.current}>
-                        {syns.current}. {synonym}
-                      </Text>
-                    )
-                  })
-                ) : (
-                  <Text>No Synonyms Found</Text>
-                )}
-                <Text style={styles.text.subHeading}>Antonyms : </Text>
-                {antonyms.length > 0 ? (
-                  antonyms.map((antonym) => {
-                    ants.current = ants.current + 1
-                    return (
-                      <Text key={ants.current}>
-                        {ants.current}. {antonym}
-                      </Text>
-                    )
-                  })
-                ) : (
-                  <Text>No Antonyms Found</Text>
-                )}
-              </View>
-            )
-          })
+        ? data.map(
+            ({ partOfSpeech, definitions, synonyms, antonyms }: meaning) => {
+              Keys.current = Keys.current + 1
+              return (
+                <Card
+                  key={Keys.current}
+                  partOfSpeech={partOfSpeech}
+                  definitions={definitions}
+                  synonyms={synonyms}
+                  antonyms={antonyms}
+                />
+              )
+            }
+          )
         : null}
     </View>
   )
